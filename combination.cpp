0831b18a57b9ac89bfa66d78d920a79733b0eb62
IidaTakuma <ta9maxxx@gmail.com>
2020-05-10 12:33:48 +0900

    Apply linter

--- combination.cpp
+++ combination.cpp
@@ -1,7 +1,7 @@
 /**
  * created: 30.03.2020
  * reference: https://drken1215.hatenablog.com/entry/2018/06/08/210000
-**/ 
+**/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,24 +12,30 @@ const int MOD = 1'000'000'007;
 long long fac[MAX], finv[MAX], inv[MAX];
 
 // テーブルを作る前処理
-void COMinit() {
+void COMinit()
+{
     fac[0] = fac[1] = 1;
     finv[0] = finv[1] = 1;
     inv[1] = 1;
-    for(int i = 2; i < MAX; i++){
+    for (int i = 2; i < MAX; i++)
+    {
         fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
         finv[i] = finv[i - 1] * inv[i] % MOD;
     }
 }
 
-long long COM(int n, int k){
-    if(n < k) return 0;
-    if(n < 0 || k < 0) return 0;
+long long COM(int n, int k)
+{
+    if (n < k)
+        return 0;
+    if (n < 0 || k < 0)
+        return 0;
     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
-int main() {
+int main()
+{
     // 前処理
     COMinit();
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-cout << "main function called!" << endl;
+int main()
+{
+    cout << "main function called!" << endl;
 }
