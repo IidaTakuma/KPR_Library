b2ebda9028a22b9267cc5b8c005bee9785b227d6
IidaTakuma <ta9maxxx@gmail.com>
2020-08-22 14:19:54 +0900

    add combination not mod

--- combination.cpp
+++ combination.cpp
@@ -1,45 +1,30 @@
-/**
- * created: 30.03.2020
- * reference: https://drken1215.hatenablog.com/entry/2018/06/08/210000
-**/
-
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-/* MAXが入力される値より大きくなっていることを確認する */
-const int MAX = 510'000;
-const int MOD = 1'000'000'007;
-
-long long fac[MAX], finv[MAX], inv[MAX];
-
-// テーブルを作る前処理
-void COMinit()
+ll combination(int n, int r)
 {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < MAX; i++)
+    ll numerator = 1;
+    ll denominator = 1;
+    for (int i = 0; i < r; i++)
     {
-        fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-        finv[i] = finv[i - 1] * inv[i] % MOD;
+        numerator *= n - i;
+        if (numerator % (i + 1) == 0)
+        {
+            numerator /= (i + 1);
+        }
+        else
+        {
+            denominator *= (i + 1);
+        }
     }
-}
-
-long long COM(int n, int k)
-{
-    if (n < k)
-        return 0;
-    if (n < 0 || k < 0)
-        return 0;
-    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+    return numerator / denominator;
 }
 
 int main()
 {
-    // 前処理
-    COMinit();
-
-    // 計算例
-    cout << COM(100000, 50000) << endl;
+    int n, r;
+    cin >> n >> r;
+    cout << combination(n, r) << endl;
+    return 0;
 }
