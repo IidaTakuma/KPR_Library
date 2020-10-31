3d51444d24a3c389c1f10e40697f65a6e745a2f6
IIda Takuma <ta9maxxx@gmail.com>
2020-11-01 01:21:38 +0900

    refactor union_find

--- union_find.cpp
+++ union_find.cpp
@@ -1,63 +1,61 @@
-/**
- * created: 22.06.2020
- * reference: ありほん
-**/
-
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using P = pair<int, int>;
 
-const int MAX_N = 100000; // 要素数の最大
-
-int par[MAX_N]; // 親
-int siz[MAX_N]; // 木のサイズ
-
-// n要素で初期化
-void init(int n)
+class UnionFind
 {
-    for (int i = 0; i < n; i++)
+    vector<int> par; // 親(根ではない)
+    vector<int> siz; // 木のサイズ
+
+public:
+    // コンストラクタ
+    UnionFind(int n)
     {
-        par[i] = i;
-        siz[i] = 1;
+        par.assign(n, 1);
+        siz.assign(n, 1);
+        for (int i = 0; i < n; i++)
+            par[i] = i;
     }
-}
+    // 木の根を求める
+    int find(int x)
+    {
+        while (par[x] != x)
+            x = par[x] = par[par[x]];
+        return x;
+    }
 
-// 木の根を求める
-int find(int x)
-{
-    while (par[x] != x)
-        x = par[x] = par[par[x]];
-    return x;
-}
+    // xとyの属する集合を併合
+    bool unite(int x, int y)
+    {
+        x = find(x);
+        y = find(y);
+        if (x == y)
+            return false;
 
-// xとyの属する集合を併合
-bool unite(int x, int y)
-{
-    x = find(x);
-    y = find(y);
-    if (x == y)
-        return false;
+        if (siz[x] < siz[y])
+            swap(x, y);
+        siz[x] += siz[y];
+        par[y] = x;
+        return true;
+    }
 
-    if (siz[x] < siz[y])
-        swap(x, y);
-    siz[x] += siz[y];
-    par[y] = x;
-    return true;
-}
+    // 頂点xと頂点yが同じ木に属するかを判定する
+    bool same(int x, int y)
+    {
+        return find(x) == find(y);
+    }
 
-// 頂点xと頂点yが同じ木に属するかを判定する
-bool same(int x, int y)
-{
-    return find(x) == find(y);
-}
-
-// xが属する木の大きさを返す
-ll size(int x)
-{
-    return siz[find(x)];
-}
-
+    // xが属する木の大きさを返す
+    int size(int x)
+    {
+        return siz[find(x)];
+    }
+};
 int main()
 {
+    UnionFind uf(10);
+    uf.unite(1, 2);
+    assert(uf.same(0, 1) == false);
+    assert(uf.same(1, 2) == true);
+    assert(uf.find(2) == 1);
+    assert(uf.size(1) == 2);
 }
