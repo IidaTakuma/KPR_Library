a698b68f4fd436319ebdb7360a0f93995e96a082
IIda Takuma <ta9maxxx@gmail.com>
2021-01-20 03:37:55 +0900

    refactor segtree

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,129 +1,139 @@
-/**
- * created: 05.10.2020
- * reference1: https://algo-logic.info/segment-tree
- * reference2: https://atcoder.github.io/ac-library/master/document_ja/segtree.html
- * sample problem: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A&lang=jp
-**/
-
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 
 /**
- * Segtree<X>(n, fx, ex):モノイド(集合X，二項演算fx，単位元ex)についてサイズnで構築
- * set(int i, int x), build(): i番目の要素をxにセット．まとめてセグ木を構築する．O(n)
- * update(i, x): i番目の要素をxに更新．O(log(n))
- * query(a, b): [a, b)全てにfxを作用させた値を取得．O(log(n))
- * [未実装]find_rightest(a, b, x): [a, b) で{モノイドの作用}の要素を持つ最右位置を求める．O(log(n))
- * [未実装]find_leftest(a, b, x): [a, b) で{モノイドの作用}の要素を持つ最左位置を求める．O(log(n))
+ * 初期化
+    * Segtree<X, モノイド, 単位元>
+ * メンバ関数 
+    * set(i, x): i番目の要素をxに更新．O(log(n))
+    * get(i): i番目の要素を取得. O(1)
+    * prod(l, r): op([a[l]...a[r - 1])を返す. O(log(n))
+    * all_prod(): op(a[0]...a[n - 1])を返す. O(1)
 **/
-template <typename X>
+
+template <class X, X (*op)(X, X), X (*e)()>
 struct SegTree
 {
-    using FX = function<X(X, X)>; // X・X -> X となる関数の型
-    int n;
-    FX fx;
-    const X ex;
-    vector<X> dat;
-    // コンストラクタ
-    SegTree(int n_, FX fx_, X ex_)
-        : n(), fx(fx_), ex(ex_), dat(n_ * 4, ex_)
-    {
-        int x = 1;
-        while (n_ > x)
-            x *= 2;
-        n = x;
-    }
+private:
+    int _n, log, size;
+    vector<X> d;
 
-    void set(int i, X x)
+public:
+    SegTree() : SegTree(0) {}
+    explicit SegTree(int n) : SegTree(vector<X>(n, e())) {}
+    explicit SegTree(const vector<X> &v) : _n(int(v.size()))
     {
-        dat[i + n - 1] = x;
-    }
-
-    void build()
-    {
-        for (int k = n - 2; k >= 0; --k)
-            dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
-    }
-
-    void update(int i, X x)
-    {
-        i += n - 1;
-        dat[i] = x;
-        while (i > 0)
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = vector<X>(2 * size, e());
+        for (int i = 0; i < _n; ++i)
         {
-            i = (i - 1) / 2; // parent
-            dat[i] = fx(dat[i * 2 + 1], dat[i * 2 + 2]);
+            d[size + i] = v[i];
+        }
+        for (int i = size - 1; i >= 1; --i)
+        {
+            update(i);
         }
     }
 
-    X query(int a, int b)
+    void set(int p, X x)
     {
-        return query_sub(a, b, 0, 0, n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; ++i)
+            update(p >> i);
     }
 
-    X query_sub(int a, int b, int k, int l, int r)
+    X get(int p)
     {
-        if (r <= a || b <= l)
-            return ex;
-        else if (a <= l && r <= b)
-            return dat[k];
-        else
+        return d[p + size];
+    }
+
+    X prod(int l, int r)
+    {
+        assert(0 <= l && l <= r && r <= _n);
+        X sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r)
         {
-            X vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
-            X vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
-            return fx(vl, vr);
+            if (l & 1)
+                sml = op(sml, d[l++]);
+            if (r & 1)
+                smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
         }
+        return op(sml, smr);
     }
-    // int find_rightest(int a, int b, T x)
-    // {
-    //     return find_rightest_sub(a, b, x, 0, 0, n);
-    // }
 
-    // int find_leftest(int a, int b, T x)
-    // {
-    //     return find_leftest_sub(a, b, x, 0, 0, n);
-    // }
+    X all_prod()
+    {
+        return d[1];
+    }
 
-    // int find_rightest_sub(int a, int b, T x, int k, int l, int r)
-    // {
-    //     // 自分の位置がxより大きい or [a, b)が[l, r)の範囲外ならreturn a-1
-    //     if (dat[k] > x || r <= a || b <= l)
-    //         return a - 1;
-    //     else if (k >= n - 1) // 自分が葉ならその位置をreturn
-    //         return (k - (n - 1));
-    //     else
-    //     {
-    //         int vr = find_rightest_sub(a, b, x, 2 * k + 2, (l + r) / 2, r);
-    //         if (vr != a - 1) // 右の部分木を見て a-1 以外ならreturn
-    //             return vr;
-    //         else // 左の部分木を見て値をreturn
-    //             return find_rightest_sub(a, b, x, 2 * k + 1, l, (l + r) / 2);
-    //     }
-    // }
+private:
+    void update(int k)
+    {
+        d[k] = op(d[2 * k], d[2 * k + 1]);
+    }
+
+    int ceil_pow2(int n)
+    {
+        int x = 0;
+        while ((1U << x) < (unsigned int)(n))
+            x++;
+        return x;
+    }
 };
+
+// セグメント木の二項演算
+int op(int a, int b)
+{
+    return a ^ b;
+}
+
+// 単位元
+int e()
+{
+    return 0;
+}
 
 int main()
 {
-    auto fx = [](int a, int b) -> int { return min(a, b); }; // モノイド
-    int n, q;
-    cin >> n >> q;
-    SegTree<int> st(n, fx, INT32_MAX);
+    int N, Q;
+    cin >> N >> Q;
 
-    // クエリを処理する
-    for (int i = 0; i < q; ++i)
+    vector<int> a(N);
+    for (int i = 0; i < N; ++i)
     {
-        int mode, x, y;
-        cin >> mode >> x >> y;
-        if (mode == 0)
+        cin >> a[i];
+    }
+
+    SegTree<int, op, e> seg(a);
+
+    for (int q = 0; q < Q; ++q)
+    {
+        int t, x, y;
+        cin >> t >> x >> y;
+        --x;
+        if (t == 1)
         {
-            st.update(x, y);
+            // 元の値
+            int v = seg.get(x);
+
+            // 変更後の値
+            v ^= y;
+
+            // 更新
+            seg.set(x, v);
         }
-        else if (mode == 1)
+        else
         {
-            cout << st.query(x, y + 1) << endl;
+            cout << seg.prod(x, y) << endl;
         }
     }
-    return 0;
 }
